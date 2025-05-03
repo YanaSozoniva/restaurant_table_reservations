ad454a8ccecef76c427d7b169fa2f63abc33cc88
Yana Sozonova <ynakochneva@mail.ru>
2025-05-03 19:35:08 +0500

    добавление формы для брони и валидация в ней

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+
+from django.utils import timezone
 from django import forms
 from django.core.validators import EmailValidator
 from phonenumber_field.formfields import PhoneNumberField
+from reservation.models import Reservation
 
 from users.forms import StyleFormMixin
 
@@ -10,3 +14,26 @@ class ContactForm(StyleFormMixin, forms.Form):
     email = forms.EmailField(validators=[EmailValidator()])
     phone = PhoneNumberField(required=False)
     message = forms.CharField(widget=forms.Textarea)
+
+
+class ReservationForm(forms.ModelForm):
+    model = Reservation
+    exclude = ("owner",)
+
+    def clean_date_reservation(self):
+        """Валидация проверки даты бронирования столика"""
+        cleaned_data = super().clean()
+        date_reservation = cleaned_data.get("date_reservation")
+        today = datetime.now().today()
+
+        if date_reservation >= today:
+            self.add_error("date_reservation", "Дата бронирования не может быть позже сегодняшней даты")
+
+    def clean_time_reservation(self):
+        """Валидация проверки время бронирования столика"""
+        time_reservation = self.cleaned_data.get("time_reservation")
+        now = timezone.now()
+        time_plus_2h = (now + timezone.timedelta(hours=2)).time()
+
+        if time_reservation > time_plus_2h:
+            self.add_error("time_reservation", "Бронирование доступно за 2 часа до назначенного часа")

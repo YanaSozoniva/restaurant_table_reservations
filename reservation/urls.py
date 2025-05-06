224ecd50348caad84e306de804fdb22876ac7085
Yana Sozonova <ynakochneva@mail.ru>
2025-05-06 13:07:02 +0500

    добавлены истории бронирования (reservation_list), детализации (reservation_detail) и вьюшек к ним и урлыБ шаблоны

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from reservation.apps import ReservationConfig
-from reservation.views import HomeViews, AboutRestaurantViews, ReservationCreate, TableCreate, TableList, TableDetail, TableUpdate, TableDelete
+from reservation.views import HomeViews, AboutRestaurantViews, ReservationCreate, TableCreate, TableList, TableDetail, TableUpdate, TableDelete, ReservationDetail, ReservationList
 
 app_name = ReservationConfig.name
 
 urlpatterns = [
     path("", HomeViews.as_view(), name="home"),
     path("restaurant/", AboutRestaurantViews.as_view(), name="restaurant"),
-    path("reservation/", ReservationCreate.as_view(), name="reservation_create"),
+    path("reservation/create/", ReservationCreate.as_view(), name="reservation_create"),
+    path("reservation/<int:pk>/", ReservationDetail.as_view(), name="reservation_detail"),
+    path("reservation/", ReservationList.as_view(), name="reservation_list"),
     path("tables/", TableList.as_view(), name="table_list"),
     path("table/<int:pk>/", TableDetail.as_view(), name="table_detail"),
     path("table/create/", TableCreate.as_view(), name="table_create"),
